78c2cb238fe685be51045ccfb5059b12739797fb
Konrad Malkowski <konrad.malkowski@gmail.com>
2019-10-08 22:53:56 -0400

    feat: obstacles sent to teb

--- src/obstacle_adapter.cpp
+++ src/obstacle_adapter.cpp
@@ -34,14 +34,17 @@
  */
 
 #include <srslib_framework/MsgTrackingObstacles.h>
+#include <costmap_converter/ObstacleArrayMsg.h>
+#include <costmap_converter/ObstacleMsg.h>
 #include "obstacle_detector/obstacle_adapter.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 using namespace std;
 using namespace obstacle_detector;
 
 ObstacleAdapter::ObstacleAdapter(ros::NodeHandle& nh, ros::NodeHandle& nh_local) : nh_(nh), nh_local_(nh_local) {
   obstacles_sub_ = nh_.subscribe("obstacles", 10, &ObstacleAdapter::publishObstaclesCallback, this);
-  obstacles_pub_ = nh_.advertise<srslib_framework::MsgTrackingObstacles>("tracked_obstacles", 10);
+  obstacles_pub_ = nh_.advertise<costmap_converter::ObstacleArrayMsg>("tracked_obstacles", 10);
 }
 
 ObstacleAdapter::~ObstacleAdapter() {
@@ -49,16 +52,35 @@ ObstacleAdapter::~ObstacleAdapter() {
 }
 
 void ObstacleAdapter::publishObstaclesCallback(const obstacle_detector::Obstacles::ConstPtr obstacles) {
-  srslib_framework::MsgTrackingObstaclesPtr obstacles_msg(new srslib_framework::MsgTrackingObstacles);
+    costmap_converter::ObstacleArrayMsgPtr obstacles_msg(new costmap_converter::ObstacleArrayMsg);
 
-  obstacles_msg->header = obstacles->header;
-  for(size_t i = 0; i < obstacles->circles.size(); i++) {
-    srslib_framework::CircularObstacle circularObstacle;
-    circularObstacle.center = obstacles->circles[i].center;
-    circularObstacle.velocity = obstacles->circles[i].velocity;
-    circularObstacle.radius = obstacles->circles[i].true_radius;
+    obstacles_msg->header = obstacles->header;
+     for(size_t i = 0; i < obstacles->circles.size(); i++) {
+         costmap_converter::ObstacleMsg circularObstacle;
+         circularObstacle.id = i;
+         circularObstacle.radius = obstacles->circles[i].true_radius;
+         geometry_msgs::Point32 center;
+         center.x = obstacles->circles[i].center.x;
+         center.y = obstacles->circles[i].center.y;
+         center.z = obstacles->circles[i].center.z;
+         circularObstacle.polygon.points.push_back(center);
 
-    obstacles_msg->trackingObstacles.push_back(circularObstacle);
+         float yaw = std::atan2(obstacles->circles[i].velocity.y, obstacles->circles[i].velocity.x);
+         tf2::Quaternion q;
+         q.setRPY(0,0,yaw);
+         circularObstacle.orientation.x = q.getX();
+         circularObstacle.orientation.y = q.getY();
+         circularObstacle.orientation.z = q.getZ();
+         circularObstacle.orientation.w = q.getW();
+
+         circularObstacle.velocities.twist.linear.x = obstacles->circles[i].velocity.x;
+         circularObstacle.velocities.twist.linear.y = obstacles->circles[i].velocity.y;
+         circularObstacle.velocities.twist.linear.z = 0;
+         circularObstacle.velocities.twist.angular.x = 0;
+         circularObstacle.velocities.twist.angular.y = 0;
+         circularObstacle.velocities.twist.angular.z = 0;
+
+        obstacles_msg->obstacles.push_back(circularObstacle);
   }
 
   obstacles_pub_.publish(obstacles_msg);
